c51030fba41b4832f2c10154a0e68d54cead83e1
dk0018w@gmail.com <dk0018w@gmail.com>
2022-08-02 10:18:01 +0530

    Achieved configuration for congestion in NewReno * Added configuration for Sender and Reciever Buffer Size * The application layer now uses and infinite bulk sender * Added configuration for Segment Size * Added DropTail queue in bottleneck PointToPoint link * Removed Netanim( for reducing runtime)

--- dumbell-ns3/tcp_ftp_n.cc
+++ dumbell-ns3/tcp_ftp_n.cc
@@ -14,148 +14,54 @@
 #include "ns3/flow-monitor-helper.h"
 #include "ns3/ipv4-global-routing-helper.h"
 #include "ns3/traffic-control-module.h"
-#include "ns3/netanim-module.h"
+//#include "ns3/netanim-module.h"
 
 
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE("TCPSCRIPT");
 
-class MyApp : public Application
-{
-    public:
-        MyApp ();
-        virtual ~MyApp();
-
-        void Setup ( Ptr<Socket> socket, Address address, uint32_t packetSize, uint32_t nPackets, DataRate dataRate);
-
-
-    private:
-        virtual void StartApplication (void);
-        virtual void StopApplication (void);
-
-        void ScheduleTx (void);
-        void SendPacket (void);
-
-
-        Ptr<Socket> m_socket;
-        Address  m_peer;
-        uint32_t m_packetSize;
-        uint32_t m_nPackets;
-        DataRate m_dataRate;
-        EventId  m_sendEvent;
-        bool     m_running;
-        uint32_t m_packetsSent;
-        
-};
-
-MyApp::MyApp ()
-    : m_socket(0),
-      m_peer(),
-      m_packetSize(0),
-      m_nPackets (0),
-      m_dataRate(0),
-      m_sendEvent(),
-      m_running(false),
-      m_packetsSent(0)
-{
-}
-
-
-MyApp::~MyApp()
-{
-    m_socket = 0;
-}
-
-
-void
-MyApp::Setup (Ptr<Socket> socket, Address address, uint32_t packetSize, uint32_t nPackets, DataRate dataRate)
-{
-    m_socket = socket;
-    m_peer = address;
-    m_packetSize = packetSize;
-    m_nPackets = nPackets;
-    m_dataRate = dataRate;
-
-}
-
-void
-MyApp::StartApplication (void)
-{
-    m_running = true;
-    m_packetsSent = 0;
-    m_socket->Bind();
-    m_socket->Connect(m_peer);
-    SendPacket();
-}
-
-void 
-MyApp::StopApplication (void)
-{
-    m_running = false;
-    if(m_sendEvent.IsRunning())
-    {
-        Simulator::Cancel ( m_sendEvent );
-    }
-
-    if (m_socket)
-    {
-        m_socket->Close();
-    }
-    
-}
-
-
-
-void
-MyApp::SendPacket(void)
-{
-    Ptr<Packet> packet = Create<Packet> (m_packetSize);
-    m_socket->Send(packet);
-    ++m_packetsSent;
-    ScheduleTx();
-    // if ( ++m_packetsSent < m_nPackets ){
-    //     ScheduleTx();
-    // }
-}
-
-void
-MyApp::ScheduleTx (void)
-{
-    if( m_running )
-    {   // tNext is the amount of time before next packet should be sent
-        Time tNext (Seconds (m_packetSize * 8 / static_cast<double> (m_dataRate.GetBitRate())));
-        // schedule the sending of another packet after tNext time.
-        m_sendEvent = Simulator::Schedule (tNext, &MyApp::SendPacket, this);
-
-    }
-}
-
 static void
-plotCwndChange (Ptr<OutputStreamWrapper> stream, uint32_t oldCwnd, uint32_t newCwnd){
+plotCwndChange (Ptr<OutputStreamWrapper> stream,float_t &prev, uint32_t oldCwnd, uint32_t newCwnd){
     //NS_LOG_UNCOND(Simulator::Now().GetSeconds() << "\t" << newCwnd);
+    // if ( Simulator::Now().GetSeconds()-prev > 0.1 ){
+    //     *stream->GetStream () << Simulator::Now().GetSeconds() << "\t" << newCwnd << std::endl;
+    //     prev += 0.1;
+    // }
     *stream->GetStream () << Simulator::Now().GetSeconds() << "\t" << newCwnd << std::endl;
 }
 
+// static void
+// RxDrop(Ptr<OutputStreamWrapper> stream,  Ptr<const Packet> p){
+//    *stream->GetStream () << "RxDrop at " << Simulator::Now().GetSeconds() << std::endl;
+// } 
+
+//Trace Congestion window length
 static void
-RxDrop(Ptr<OutputStreamWrapper> stream, Ptr<const Packet> p){
-   *stream->GetStream () << "RxDrop at " << Simulator::Now().GetSeconds() << std::endl;
-} 
+TraceCwnd (Ptr<OutputStreamWrapper> stream)
+{
+    //Trace changes to the congestion window
+    float_t prev = Simulator::Now().GetSeconds();
+    Config::ConnectWithoutContext ("/NodeList/2/$ns3::TcpL4Protocol/SocketList/0/CongestionWindow", MakeBoundCallback (&plotCwndChange,stream, prev));
+}
+
 
 int 
 main(int argc, char *argv[])
 {   
     std::string bottleneckBandwidth = "10Mbps";
     std::string bottleneckDelay = "100us";
-    std::string accessBandwidth = "10Mbps";
-    std::string accessDelay = "100us";
+    std::string accessBandwidth = "1Mbps";
+    std::string accessDelay = "10ms";
     std::string sourceRate = "2Mbps";
     std::string flavour = "TcpBic";
     std::string queueType = "DropTail";       //DropTail or CoDel
     std::string queueSize = "2p";      //in packets
     uint32_t pktSize = 1400;        //in Bytes. 1458 to prevent fragments
     uint32_t nPackets = 2000;
-    float startTime = 0.1;
+    uint32_t rcvBuff = 10000*pktSize;
+    uint32_t sndBuff = 10000*pktSize;
+    float startTime = 0;
     float simDuration = 10;        //in seconds
     uint32_t nSources = 5;
     bool isPcapEnabled = true;
@@ -181,15 +87,24 @@ main(int argc, char *argv[])
     cmd.AddValue ("nPackets", "No. of Packets to send", nPackets);
     cmd.AddValue ("nSources", "No. of sources in the dumbell topology", nSources);
     cmd.AddValue ("sourceRate", "Rate of generating packets at the application level", sourceRate);
+    cmd.AddValue ("rcvBuff", "Size of recieve buffer in Bytes", rcvBuff);
+    cmd.AddValue ("sndBuff", "Size of send buffer in Bytes", sndBuff);
     cmd.Parse (argc, argv);
    
     float stopTime = startTime + simDuration;
    
     std::string tcpModel ("ns3::"+flavour);
+    
     Config::SetDefault ("ns3::TcpL4Protocol::SocketType", StringValue(tcpModel));
-    //Config::SetDefault("ns3::TcpSocket::SegmentSize", UintegerValue (1));
+    Config::SetDefault("ns3::TcpSocket::SegmentSize", UintegerValue (1*pktSize));
     Config::SetDefault("ns3::TcpSocket::InitialCwnd", UintegerValue (1));
-    Config::SetDefault("ns3::TcpSocketBase::MaxWindowSize", UintegerValue (20*1000));
+    //Config::SetDefault("ns3::TcpSocketBase::MaxWindowSize", UintegerValue (20*1000));
+    Config::SetDefault ("ns3::TcpSocket::SndBufSize", UintegerValue (sndBuff));
+    Config::SetDefault ("ns3::TcpSocket::RcvBufSize", UintegerValue (rcvBuff));
+
+
+
+
 
     // Defining the nodes 
     NodeContainer nodes;
@@ -207,6 +122,7 @@ main(int argc, char *argv[])
     PointToPointHelper p2p;
     p2p.SetDeviceAttribute ("DataRate", StringValue(accessBandwidth));
     p2p.SetChannelAttribute ("Delay", StringValue(accessDelay));
+    p2p.SetQueue ("ns3::DropTailQueue");
     p2p.SetQueue ("ns3::DropTailQueue<Packet>", "MaxSize", QueueSizeValue (QueueSize (queueSize))); // p in 1000p stands for packets
 
 
@@ -284,52 +200,52 @@ main(int argc, char *argv[])
     }
     
     Ptr<Socket> ns3TcpSocket[nSources];
-    Ptr<MyApp> app[nSources];
+   
+    ApplicationContainer sourceApps[nSources];
 
     // Configuring the application at each source node.
     for (uint32_t i = 0; i < nSources; i++)
     {
-        ns3TcpSocket[i] = Socket::CreateSocket (nodes.Get (2+i), TcpSocketFactory::GetTypeId ());
-                
-        app[i] = CreateObject<MyApp> ();
-        app[i]->Setup (ns3TcpSocket[i], sinkAddress[i], pktSize, nPackets, DataRate (sourceRate));
-        nodes.Get (2+i)->AddApplication (app[i]);
-        app[i]->SetStartTime (Seconds (startTime));
-        app[i]->SetStopTime (Seconds (stopTime));
+        
+        BulkSendHelper tmp_source("ns3::TcpSocketFactory",InetSocketAddress (ip_d_r2[i].GetAddress (0), sinkPort));   
+        // Set the amount of data to send in bytes.  Zero is unlimited.
+        tmp_source.SetAttribute ("MaxBytes", UintegerValue (0));
+        sourceApps[i] = tmp_source.Install (nodes.Get (2+i));
+        sourceApps->Start (Seconds (startTime));
+        sourceApps->Stop (Seconds (stopTime));
     }
 
     AsciiTraceHelper ascii;
     Ptr<OutputStreamWrapper> stream = ascii.CreateFileStream ("cwnd_trace_file.txt");
     // only tracing the cwnd for a single source i.e. source -> 0 ( node - 2).
-    ns3TcpSocket[0]->TraceConnectWithoutContext ("CongestionWindow", MakeBoundCallback (&plotCwndChange, stream));
-    
-    
-    for (uint32_t i = 0; i < nSources; i++)
-    {
-        d_r2[i].Get (0)->TraceConnectWithoutContext ("PhyRxDrop", MakeBoundCallback (&RxDrop, stream));
-    }
+    Simulator::Schedule( Seconds(0.1), &TraceCwnd, stream);
+        
+    // for (uint32_t i = 0; i < nSources; i++)
+    // {
+    //     d_r2[i].Get (0)->TraceConnectWithoutContext ("PhyRxDrop", MakeBoundCallback (&RxDrop, stream));
+    // }
     
     Simulator::Stop (Seconds (stopTime));
     Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
 
-    AnimationInterface anim("tcp_n.xml");
-    anim.SetMobilityPollInterval (Seconds (1));
+    // AnimationInterface anim("tcp_ftp_n.xml");
+    // anim.SetMobilityPollInterval (Seconds (1));
 
-    if ( nSources == 2){
-          // // X (->) , Y(|) 
-        // // source nodes
-        anim.SetConstantPosition(nodes.Get(2), 10.0, 20.0);
-        anim.SetConstantPosition(nodes.Get(3), 10.0, 40.0);
+    // if ( nSources == 2){
+    //       // // X (->) , Y(|) 
+    //     // // source nodes
+    //     anim.SetConstantPosition(nodes.Get(2), 10.0, 20.0);
+    //     anim.SetConstantPosition(nodes.Get(3), 10.0, 40.0);
 
-        // // // router nodes
-        anim.SetConstantPosition(nodes.Get(0), 30.0, 30.0);
-        anim.SetConstantPosition(nodes.Get(1), 50.0, 30.0);
+    //     // // // router nodes
+    //     anim.SetConstantPosition(nodes.Get(0), 30.0, 30.0);
+    //     anim.SetConstantPosition(nodes.Get(1), 50.0, 30.0);
 
-        // // // sink nodes
-        anim.SetConstantPosition(nodes.Get(4), 70.0, 20.0);
-        anim.SetConstantPosition(nodes.Get(5), 70.0, 40.0);
-    }
-    anim.SetMaxPktsPerTraceFile(50000000000);
+    //     // // // sink nodes
+    //     anim.SetConstantPosition(nodes.Get(4), 70.0, 20.0);
+    //     anim.SetConstantPosition(nodes.Get(5), 70.0, 40.0);
+    // }
+    // anim.SetMaxPktsPerTraceFile(50000000000);
     
     
     Simulator::Run ();
